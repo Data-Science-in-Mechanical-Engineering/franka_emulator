54b9a0a33b0cc1d900d087d411abd4a422b2b59a
Kyrylo Sovailo <k.sovailo@gmail.com>
2021-10-28 20:07:45 +0200

    First working version

--- example/selftest.cpp
+++ example/selftest.cpp
@@ -3,7 +3,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-static const double tolerance = 0.001;
+static const double relative_tolerance = 0.5;
 static std::string real_ip;
 franka::Model *real_model;
 FRANKA_EMULATOR_CXX_NAME::Model *emulator_model;
@@ -20,9 +20,17 @@ TEST(Model, Gravity)
     std::array<double, 3> F = { 0.0, 0.0, 0.0 };
     std::array<double, 7> real_gravity;
     EXPECT_NO_THROW(real_gravity = real_model->gravity(q, m, F));
+    std::cout << "Real gravity    :"; for (size_t i = 0; i < 7; i++) std::cout << " " << real_gravity[i]; std::cout << std::endl;
     std::array<double, 7> emulator_gravity;
     EXPECT_NO_THROW(emulator_gravity = emulator_model->gravity(q, m, F));
-    for (size_t i = 0; i < 7; i++) EXPECT_NEAR(real_gravity[i], emulator_gravity[i], tolerance);
+    std::cout << "Emulator gravity:"; for (size_t i = 0; i < 7; i++) std::cout << " " << emulator_gravity[i]; std::cout << std::endl;
+    EXPECT_NEAR(real_gravity[0], emulator_gravity[0], abs(real_gravity[0]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[1], emulator_gravity[1], abs(real_gravity[1]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[2], emulator_gravity[2], abs(real_gravity[2]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[3], emulator_gravity[3], abs(real_gravity[3]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[4], emulator_gravity[4], abs(real_gravity[4]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[5], emulator_gravity[5], abs(real_gravity[5]) * relative_tolerance);
+    EXPECT_NEAR(real_gravity[6], emulator_gravity[6], abs(real_gravity[6]) * relative_tolerance);
 }
 
 TEST(Model, Coriolis)
@@ -34,9 +42,17 @@ TEST(Model, Coriolis)
     std::array<double, 3> F = { 0.0, 0.0, 0.0 };
     std::array<double, 7> real_coriolis;
     EXPECT_NO_THROW(real_coriolis = real_model->coriolis(q, dq, I, m, F));
+    std::cout << "Real coriolis    :"; for (size_t i = 0; i < 7; i++) std::cout << " " << real_coriolis[i]; std::cout << std::endl;
     std::array<double, 7> emulator_coriolis;
     EXPECT_NO_THROW(emulator_coriolis = emulator_model->coriolis(q, dq, I, m, F));
-    for (size_t i = 0; i < 7; i++) EXPECT_NEAR(real_coriolis[i], emulator_coriolis[i], tolerance);
+    std::cout << "Emulator coriolis:"; for (size_t i = 0; i < 7; i++) std::cout << " " << emulator_coriolis[i]; std::cout << std::endl;
+    EXPECT_NEAR(real_coriolis[0], emulator_coriolis[0], abs(real_coriolis[0]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[1], emulator_coriolis[1], abs(real_coriolis[1]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[2], emulator_coriolis[2], abs(real_coriolis[2]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[3], emulator_coriolis[3], abs(real_coriolis[3]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[4], emulator_coriolis[4], abs(real_coriolis[4]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[5], emulator_coriolis[5], abs(real_coriolis[5]) * relative_tolerance);
+    EXPECT_NEAR(real_coriolis[6], emulator_coriolis[6], abs(real_coriolis[6]) * relative_tolerance);
 }
 
 int main(int argc, char **argv)
