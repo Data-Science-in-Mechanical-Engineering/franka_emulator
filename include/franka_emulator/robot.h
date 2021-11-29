007ac272efafff0bc3d549e5f79e5d3536542850
Kyrylo Sovailo <k.sovailo@gmail.com>
2021-11-29 04:39:18 +0100

    Installation fixed, README.md and LICENSE.md added

--- include/franka_emulator/robot.h
+++ include/franka_emulator/robot.h
@@ -708,3 +708,95 @@ private:
 };
 
 }  // namespace FRANKA_EMULATOR
+
+/** @mainpage Welcome to `franka_emulator 1.0.0`!
+
+Here you will find a `C++` library that replaces and behaves like [libfranka](https://frankaemika.github.io/docs/libfranka.html) with one exception - the robot is virtual! It has similar `C++` and `CMake` API and is created to give developers a possibility to write programs for Panda robot without having it.
+
+@tableofcontents
+
+@section Usage
+Usage `franka_emulator` is supposed to be very similar to usage of [libfranka](https://frankaemika.github.io/docs/libfranka.html). Simplest `C++` program reads:
+```
+#include <franka_emulator/robot.h>
+int main()
+{
+	franka_emulator::Robot robot("192.168.0.1");
+}
+```
+While typical `CMakeLists.txt` usage looks like:
+```
+find_package(FrankaEmulator 1.0.0 REQUIRED)
+add_executable(example example.cpp)
+target_link_libraries(example PRIVATE Franka::FrankaEmulator)
+```
+And that's it, no further changes are needed. And is that is not enough, you can add `-DFRANKA_EMULATOR_IMITATE` to `cmake` options to eliminate even these differences!
+
+The simulation itself can be launched with:
+```
+./franka_emulator.sh <IP>
+```
+Where `<IP>` is the same string that needs to be passed to `franka_emulator::Robot` to make it connected to the simulation. The string is arbitrary (but without slashes) as it is not an actual `IP` address. Actually, the emulator does use sockets at all.
+
+@section Requirements
+The library requires:
+ - [Gazebo](http://gazebosim.org) (Standalone or as part of `ROS`)
+ - [pinocchio](https://stack-of-tasks.github.io/pinocchio) (set with `-Dpinocchio_DIR=/absolute_path_to_pinocchio` or as part of `ROS`)
+ - [Eigen](https://eigen.tuxfamily.org)
+ - [libfranka](https://github.com/frankaemika/libfranka) (optionally) (set with `-DFranka_DIR=/absolute_path_to_libfranka/build` or as part of `ROS`)
+ - [Google Test](https://github.com/google/googletest) (optionally)
+ - [Doxygen](https://www.doxygen.nl/index.html) (optionally)
+ - [CMake](https://cmake.org) >= `3.4.0`
+ - C++11 compatible compiler
+ - Fully preemptable Linux kernel (optionally)
+
+@section Building
+The library can be built with [CMake](https://cmake.org) using the following commands:
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+
+@section Installation
+**Optional step 1**: Installing the library itself:
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+sudo cmake --install .
+sudo ldconfig
+```
+
+**Step 2**: Allowing yourself to use real-time commands:
+```
+sudo addgroup realtime
+sudo usermod -a -G realtime $(whoami)
+echo @realtime soft rtprio 99       >> /etc/security/limits.conf
+echo @realtime soft priority 99     >> /etc/security/limits.conf
+echo @realtime soft memlock 102400  >> /etc/security/limits.conf
+echo @realtime hard rtprio 99       >> /etc/security/limits.conf
+echo @realtime hard priority 99     >> /etc/security/limits.conf
+echo @realtime hard memlock 102400  >> /etc/security/limits.conf
+```
+Reboot.
+
+**Optional step 3**: Installing real-time kernel
+It is said that the fully preemptive kernel can do priority boosting with semaphores, which might increase the emulator's performance (because the library uses semaphores a lot). Note that real-time kernel might cause system instability (and may not).
+
+For `Debian`: `sudo apt-get install linux-image-rt-amd64` (in case you have an x86_64 machine, proceed [here](https://packages.debian.org/search?keywords=linux-image-rt&searchon=names&suite=stable&section=all) if not)
+
+For `Arch`: `yay -S linux-rt-lts` (in case you use `yay`, proceed [here](https://wiki.archlinux.org/title/AUR_helpers) to see all options)
+
+For `Ubuntu`: **under development**
+
+@section Documentation
+As the library has the same API as [libfranka](https://frankaemika.github.io/docs/index.html#), you can proceed [here](https://frankaemika.github.io/libfranka/) to read the original version. But you also can create documentation with [Doxygen](https://www.doxygen.nl) using `doxygen` command.
+
+@section Contributors
+ - Kyrylo Sovailo
+ - Original [libfranka](https://github.com/intelligent-soft-robots/o80) is owned by [Franka Emika GmbH](https://www.franka.de)
+ - Models from `model` directory were initially generated with [Gazebo](http://gazebosim.org) from `ROS` package `franka_description`
+*/
--- source/emulator/franka_emulator.sh
+++ source/emulator/franka_emulator.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIRECTORY=$(dirname "${SCRIPT_PATH}")
 if [ $# != 1 ]; then
 	echo 'Usage: franka_emulator.sh <IP>'
-elif [ -f $(pwd)/../share/franka_emulator/model/franka_emulator.world ]; then
+elif [ -f ${SCRIPT_DIRECTORY}/../share/franka_emulator/model/franka_emulator.world ]; then
 	# Installed
-	export GAZEBO_MODEL_PATH=$(pwd)/../share/franka_emulator/model
-	export GAZEBO_PLUGIN_PATH=$(pwd)/../lib
+	export GAZEBO_MODEL_PATH=${SCRIPT_DIRECTORY}/../share/franka_emulator/model
+	export GAZEBO_PLUGIN_PATH=${SCRIPT_DIRECTORY}/../lib
+	export LD_LIBRARY_PATH=${SCRIPT_DIRECTORY}/../lib
 	export FRANKA_EMULATOR_IP=$1
-	chrt -f 90 gazebo $(pwd)/../share/franka_emulator/model/franka_emulator.world -u
+	chrt -f 90 gazebo ${SCRIPT_DIRECTORY}/../share/franka_emulator/model/franka_emulator.world -u
+elif [ -f ${SCRIPT_DIRECTORY}/model/franka_emulator.world ]; then
+	# Not installed, in-directory build
+	export GAZEBO_MODEL_PATH=${SCRIPT_DIRECTORY}/model
+	export GAZEBO_PLUGIN_PATH=${SCRIPT_DIRECTORY}
+	export LD_LIBRARY_PATH=${SCRIPT_DIRECTORY}
+	export FRANKA_EMULATOR_IP=$1
+	chrt -f 90 gazebo ${SCRIPT_DIRECTORY}/model/franka_emulator.world -u
+elif [ -f ${SCRIPT_DIRECTORY}/../model/franka_emulator.world ]; then
+	# Not installed, build in child directory
+	export GAZEBO_MODEL_PATH=${SCRIPT_DIRECTORY}/../model
+	export GAZEBO_PLUGIN_PATH=${SCRIPT_DIRECTORY}
+	export LD_LIBRARY_PATH=${SCRIPT_DIRECTORY}
+	export FRANKA_EMULATOR_IP=$1
+	chrt -f 90 gazebo ${SCRIPT_DIRECTORY}/../model/franka_emulator.world -u
 else
-	# Not installed
-	export GAZEBO_MODEL_PATH=@CMAKE_SOURCE_DIR@/model
-	export GAZEBO_PLUGIN_PATH=$(pwd)
-	export FRANKA_EMULATOR_IP=$1
-	chrt -f 90 gazebo @CMAKE_SOURCE_DIR@/model/franka_emulator.world -u
+	echo "Could not find model directory"
 fi
